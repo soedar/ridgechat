0e5741a16818eed655b7bfcf0ce03e51ab35a7a4
Soedar <soedar.sg@gmail.com>
2013-07-04 15:52:54 -0700

    Added GET post functionality. This will be modified to post once the client app is built

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var port = 22222;
 
 var RoomManager = require("./models/room_manager").RoomManager;
+var Message = require("./models/message").Message;
 
 app.get("/", function(req, res) {
     res.send("It Works");
@@ -31,6 +32,32 @@ app.get("/room/:room_id/messages", function(req, res) {
         output = {"success": false};
     }
     res.send(output);
+});
+
+app.get("/room/:room_id/post/:user_id/:message", function(req, res) {
+    var room = RoomManager.roomList[req.params.room_id];
+    if (!room) {
+        res.send({"success": false});
+        return;
+    }
+
+    var user_id = req.params.user_id;
+    var userInChat = false;
+    for (var i=0;i<room.user_ids.length;i++) {
+        if (room.user_ids[i] == user_id) {
+            userInChat = true;
+            break;
+        }
+    }
+
+    if (!userInChat) {
+        res.send({"success": false});
+        return;
+    }
+
+    var message = new Message(user_id, req.params.message);
+    room.addMessage(message);
+    res.send({"success": true});
 });
 
 app.listen(port);
--- models/message.js
+++ models/message.js
@@ -1,4 +1,4 @@
-Message = function(message) {
+Message = function(user_id, message) {
     this.user_id = user_id;
     this.message = message;
     this.timestamp = (new Date()).getTime();
