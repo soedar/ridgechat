14ce8144455477dde35c5509e2a855450562c2e1
Soedar <soedar.sg@gmail.com>
2013-07-06 12:52:10 -0700

    Reduced the timeout values

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 Config = function() {
     this.port = process.env.PORT || 22222;  // for Heroku deployment
-    this.messageTimeout = 10000;            // Determine how long to hold the http response
-    this.inactiveTimeout = 12000;           // Determine the minimum time between request that a player has to make. Should be higher that the previous value
+    this.messageTimeout = 5000;             // Determine how long to hold the http response
+    this.inactiveTimeout = 7000;            // Determine the minimum time between request that a player has to make. Should be higher that the previous value
 }
 
 exports.Config = new Config();
