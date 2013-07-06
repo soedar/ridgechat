d7bf1097a2e55f30ce696dbefe5f6f6d67229129
Soedar <soedar.sg@gmail.com>
2013-07-05 22:59:54 -0700

    Changed the timing for message timeout and inactive timeout

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 Config = function() {
     this.port = process.env.PORT || 22222;  // for Heroku deployment
-    this.messageTimeout = 3000;             // Determine how long to hold the http response
-    this.inactiveTimeout = 8000;            // Determine the minimum time between request that a player has to make. Should be higher that the previous value
+    this.messageTimeout = 15000;            // Determine how long to hold the http response
+    this.inactiveTimeout = 20000;           // Determine the minimum time between request that a player has to make. Should be higher that the previous value
 }
 
 exports.Config = new Config();
